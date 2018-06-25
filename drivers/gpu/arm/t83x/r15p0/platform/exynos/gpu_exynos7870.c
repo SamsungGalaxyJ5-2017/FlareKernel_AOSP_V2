21000e6b1975a704379a27b767aafda74f35439c
SharkEzz <icraft640@gmail.com>
2018-06-25 12:43:19 +0200

    Add CPU & GPU OC
    Signed-off-by: SharkEzz <icraft640@gmail.com>

--- drivers/gpu/arm/t83x/r15p0/platform/exynos/gpu_exynos7870.c
+++ drivers/gpu/arm/t83x/r15p0/platform/exynos/gpu_exynos7870.c
@@ -73,16 +73,16 @@ void __iomem *g3d1_outstanding_regs;
 
 /*  clk,vol,abb,min,max,down stay, pm_qos mem, pm_qos int, pm_qos cpu_kfc_min, pm_qos cpu_egl_max */
 static gpu_dvfs_info gpu_dvfs_table_default[] = {
-	{1300, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1246, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1146, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1001, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{845,  900000, 0, 78,  98, 1, 0, 836000, 400000, 1248000, CPU_MAX},
-	{728,  900000, 0, 78,  85, 1, 0, 728000, 400000, 1144000, CPU_MAX},
-	{676,  900000, 0, 78,  85, 1, 0, 676000, 400000,  902000, CPU_MAX},
-	{545,  900000, 0, 78,  85, 1, 0, 546000, 334000,  902000, CPU_MAX},
-	{450,  900000, 0, 78,  85, 1, 0, 451000, 200000,       0, CPU_MAX},
-	{343,  900000, 0, 78,  85, 1, 0, 275000, 134000,       0, CPU_MAX},
+	{1300, 965000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1246, 920000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1146, 900000, 0, 98, 100, 1, 0, 1146000, 400000, 1586000, CPU_MAX},
+	{1001, 880000, 0, 98, 100, 1, 0, 1001000, 400000, 1586000, CPU_MAX},
+	{845,  870000, 0, 78,  98, 1, 0, 836000, 400000, 1248000, CPU_MAX},
+	{728,  860000, 0, 78,  85, 1, 0, 728000, 400000, 1144000, CPU_MAX},
+	{676,  850000, 0, 78,  85, 1, 0, 676000, 400000,  902000, CPU_MAX},
+	{545,  840000, 0, 78,  85, 1, 0, 546000, 334000,  902000, CPU_MAX},
+	{450,  830000, 0, 78,  85, 1, 0, 451000, 200000,       0, CPU_MAX},
+	{343,  830000, 0, 78,  85, 1, 0, 275000, 134000,       0, CPU_MAX},
 };
 
 static int mif_min_table[] = {
@@ -94,8 +94,8 @@ static int mif_min_table[] = {
 };
 
 static gpu_attribute gpu_config_attributes[] = {
-	{GPU_MAX_CLOCK, 1001},
-	{GPU_MAX_CLOCK_LIMIT, 1001},
+	{GPU_MAX_CLOCK, 1146},
+	{GPU_MAX_CLOCK_LIMIT, 1146},
 	{GPU_MIN_CLOCK, 343},
 	{GPU_DVFS_START_CLOCK, 343},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 343},
@@ -119,7 +119,7 @@ static gpu_attribute gpu_config_attributes[] = {
 	{GPU_COLD_MINIMUM_VOL, 0},
 	{GPU_VOLTAGE_OFFSET_MARGIN, 25000},
 	{GPU_TMU_CONTROL, 1},
-	{GPU_TEMP_THROTTLING1, 1001},
+	{GPU_TEMP_THROTTLING1, 1146},
 	{GPU_TEMP_THROTTLING2, 845},
 	{GPU_TEMP_THROTTLING3, 728},
 	{GPU_TEMP_THROTTLING4, 545},
